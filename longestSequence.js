15b9bf053bdd91c9fed168ab5e3407bdde6aa67a
Stuart Mees <stuartmees@gmail.com>
2020-07-13 14:11:50 +0100

    largest char freq object made

--- longestSequence.js
+++ longestSequence.js
@@ -1,5 +1,41 @@
-const longestSequence = (a, b) => {
-    return a + b;
+const longestSequence = (seq) => {
+  
+    let seqArray = [...seq]
+    let charFreqs = {}
+    let largestCharFreqs = {}
+    let largestFreq = 0
+
+    seqArray.forEach((char, i) => {
+        if (i === 0) {
+          charFreqs[char] = [1]
+        } else if (seqArray[i-1] === char) {
+            charFreqs[char][charFreqs[char].length-1] = charFreqs[char][charFreqs[char].length-1] + 1
+        } else if (seqArray[i-1] !== char  &&  !Object.keys(charFreqs).includes(char) ){
+          charFreqs[char] = [1]
+        } else if (seqArray[i-1] !== char  &&  Object.keys(charFreqs).includes(char) ) {
+          charFreqs[char].push(1)
+        }
+    })
+    console.log(charFreqs)
+
+    Object.keys(charFreqs).forEach((char,i) => {
+        largestCharFreqs[char] = 0
+
+        charFreqs[char].forEach(freq => {
+            if (freq > largestCharFreqs[char]){
+              largestCharFreqs[char]=freq
+            } 
+        })
+    })
+
+    console.log(largestCharFreqs)
+
+
   }
 
+  longestSequence('aaabbcdddddaadddddddddd')
+
   module.exports = longestSequence;
+
+
+  // (Object.keys(charFreqs).includes(char)) && 
--- tests/longestSequence.test.js
+++ tests/longestSequence.test.js
@@ -1,5 +1,8 @@
 const longestSequence = require('../longestSequence');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(longestSequence(1, 2)).toBe(3);
+  expect(longestSequence(1, 2)).toStrictEqual({ a: 1, b: 2 });
 });
+
+
+
