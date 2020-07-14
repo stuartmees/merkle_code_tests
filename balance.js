e5a5cad5cfbf0f6b3983b4dc77979f0390c9312e
Stuart Mees <stuartmees@gmail.com>
2020-07-14 10:12:33 +0100

    update

--- balance.js
+++ balance.js
@@ -1,4 +1,5 @@
 const calcEndNetBalance = (startMonthOpeningSum, interestRate, taxFreeLimit, taxRate, monthsComplete, numMonths) => {
+
     const oneMonthInterest = startMonthOpeningSum * (interestRate/100)
     const endMonthGrossBalance = startMonthOpeningSum + oneMonthInterest
     const monthTax = endMonthGrossBalance > taxFreeLimit ? (taxRate/100) * (endMonthGrossBalance-taxFreeLimit) : 0
@@ -12,10 +13,13 @@ const calcEndNetBalance = (startMonthOpeningSum, interestRate, taxFreeLimit, tax
 const balance = (openingSum, interestRate, taxFreeLimit, taxRate, numMonths) => {
 
     const endNetBalance = calcEndNetBalance(openingSum, interestRate, taxFreeLimit, taxRate, 0, numMonths)
-    console.log(endNetBalance)
+
     return endNetBalance
 
 }
+
+console.log(balance(10000, 1, 20000, 1, 2))
+console.log(balance(25000, 2, 20000, 1, 2))
 
 module.exports = balance;
 
